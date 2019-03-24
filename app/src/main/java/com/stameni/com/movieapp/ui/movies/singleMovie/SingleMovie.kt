6778f373ce0062709180519187adc26aa4fe8a57
Mihajlo S <mihajlo.stamenkovic95@gmail.com>
2019-03-24 11:12:34 +0100

    Refactores SingleMovie activity, removed unused code, limited images in carouselview.

--- app/src/main/java/com/stameni/com/movieapp/ui/movies/singleMovie/SingleMovie.kt
+++ app/src/main/java/com/stameni/com/movieapp/ui/movies/singleMovie/SingleMovie.kt
@@ -11,6 +11,7 @@ import com.stameni.com.movieapp.R
 import kotlinx.android.synthetic.main.activity_single_movie.*
 
 const val MOVIE_ID = "movie_id"
+const val MAX_IMAGE_COUNT = 15
 
 class SingleMovie : AppCompatActivity() {
 
@@ -26,19 +27,10 @@ class SingleMovie : AppCompatActivity() {
         tabs_main.setupWithViewPager(viewpager_main)
         movieId = intent!!.extras!!.getInt(MOVIE_ID, 0)
         viewModel = ViewModelProviders.of(this).get(SingleMovieViewModel::class.java)
-
-        viewModel.loadMovie(movieId)
-        viewModel.loadClips(movieId)
-        viewModel.loadActors(movieId)
-        viewModel.loadReviews(movieId)
         viewModel.loadImages(movieId)
 
         initErrorHandling()
         initCarouselView()
-//        initClipRecyclerView()
-//        initActorsRecyclerView()
-//        initReviewsRecyclerView()
-        initFields()
     }
 
     private fun initCarouselView() {
@@ -50,7 +42,11 @@ class SingleMovie : AppCompatActivity() {
                         .resize(0, it[position].height)
                         .into(imageView)
                 }
-                myImageView.pageCount = it.size
+                if(it.size < MAX_IMAGE_COUNT) {
+                    myImageView.pageCount = it.size
+                }else{
+                    myImageView.pageCount = MAX_IMAGE_COUNT
+                }
             }
         })
     }
@@ -64,62 +60,10 @@ class SingleMovie : AppCompatActivity() {
         })
     }
 
-//    private fun initClipRecyclerView() {
-//        clip_list_rv.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL, false)
-//        clip_list_rv.adapter = viewModel.clipListAdapter
-//    }
-//
-//    private fun initActorsRecyclerView() {
-//        actor_list_rv.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL, false)
-//        actor_list_rv.adapter = viewModel.actorListAdapter
-//    }
-//
-//    private fun initReviewsRecyclerView() {
-//        review_list_rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-//        review_list_rv.adapter = viewModel.actorListAdapter
-//    }
-
-    private fun initFields() {
-        viewModel.singleMovie.observe(this, Observer { movie ->
-            if (movie != null) {
-                var genre = ""
-                movie.genres.forEach {
-                    genre += it.name + " | "
-                }
-//                title_tv.text = movie.originalTitle
-//                movie_summary.text = movie.overview
-//                rating_tv.text = movie.voteAverage.toString()
-//                release_date_tv.text = movie.releaseDate
-//                genres.text = " | $genre"
-                val url = "https://image.tmdb.org/t/p/w500/${movie.backdropPath}"
-                val backgroundUrl = "https://image.tmdb.org/t/p/w500/${movie.posterPath}"
-
-//                Picasso.get()
-//                    .load(backgroundUrl)
-//                    .transform(BlurTransformation(this, 10))
-//                    .fit()
-//                    .centerInside()
-//                    .into(backgroundImage)
-            }
-        })
-
-//        viewModel.reviewsVisibility.observe(this, Observer {
-//            if (it != null) {
-//                if (it == View.GONE) {
-//                    review_list_rv.visibility = View.GONE
-//                    review_tv.visibility = View.GONE
-//                }
-//            }
-//        })
-    }
-
     private fun showError(@StringRes errorMessage: Int) {
         errorSnackbar = Snackbar.make(mRootView, errorMessage, Snackbar.LENGTH_INDEFINITE)
         errorSnackbar?.setAction(R.string.retry) {
-            viewModel.loadMovie(movieId)
-            viewModel.loadClips(movieId)
-            viewModel.loadActors(movieId)
-            viewModel.loadReviews(movieId)
+            viewModel.loadImages(movieId)
         }
         errorSnackbar?.show()
     }
--- app/src/main/java/com/stameni/com/movieapp/ui/movies/singleMovie/SingleMovieViewModel.kt
+++ app/src/main/java/com/stameni/com/movieapp/ui/movies/singleMovie/SingleMovieViewModel.kt
@@ -20,7 +20,6 @@ import javax.inject.Inject
 
 class SingleMovieViewModel : BaseViewModel() {
 
-
     @Inject
     lateinit var movieApi: TMDBApi
 
@@ -28,63 +27,7 @@ class SingleMovieViewModel : BaseViewModel() {
 
     val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
     val errorMessage: MutableLiveData<Int> = MutableLiveData()
-    val reviewsVisibility: MutableLiveData<Int> = MutableLiveData()
-    val singleMovie: MutableLiveData<SingleMovieResponse> = MutableLiveData()
     val movieImages: MutableLiveData<ArrayList<Backdrop>> = MutableLiveData()
-
-    val clipListAdapter: ClipListAdapter = ClipListAdapter()
-    val actorListAdapter: ActorListAdapter = ActorListAdapter()
-    val movieReviewsAdapter: MovieReviewsAdapter = MovieReviewsAdapter()
-
-    fun loadMovie(id: Int) {
-        subscription = movieApi.getSingleMovie(id)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { onRetrieveMovieStart() }
-            .doOnTerminate { onRetrieveMovieFinish() }
-            .subscribe(
-                { result ->
-                    onRetrievePostMovieSuccess(result)
-                },
-                { onRetrievePostMovieError() }
-            )
-    }
-
-    fun loadClips(id: Int) {
-        subscription = movieApi.getMovieClips(id)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { result ->
-                    onRetrieveClipSuccess(result)
-                },
-                { onRetrieveClipError() }
-            )
-    }
-
-    fun loadActors(id: Int){
-        subscription = movieApi.getMovieActors(id)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { result ->
-                    onRetrieveActorsSuccess(result)
-                },
-                { onRetrieveActorsError() }
-            )
-    }
-
-    fun loadReviews(id: Int, page: Int = 1){
-        subscription = movieApi.getMovieReviews(id, page)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { result ->
-                    onRetrieveReviewsSuccess(result)
-                },
-                { onRetrieveReviewsError() }
-            )
-    }
 
     fun loadImages(id: Int){
         subscription = movieApi.getMovieImages(id)
@@ -101,56 +44,9 @@ class SingleMovieViewModel : BaseViewModel() {
 
     private fun onRetrieveImagesError(error: Throwable) {
         errorMessage.value = R.string.movie_images_error
-        println(error)
     }
 
     private fun onRetrieveImagesSuccess(result: ArrayList<Backdrop>) {
         movieImages.value = result
     }
-
-    private fun onRetrieveReviewsError() {
-        errorMessage.value = R.string.review_error
-    }
-
-    private fun onRetrieveReviewsSuccess(result: MovieReviewResponse) {
-        if(result.results.isEmpty())
-            reviewsVisibility.value = View.GONE
-        movieReviewsAdapter.setReviewList(result)
-    }
-
-    private fun onRetrieveActorsError() {
-        errorMessage.value = R.string.actors_error
-    }
-
-    private fun onRetrieveActorsSuccess(result: MovieActorsResponse) {
-        errorMessage.value = null
-        actorListAdapter.setActorList(result)
-    }
-
-    private fun onRetrieveClipError() {
-        errorMessage.value = R.string.clip_error
-    }
-
-    private fun onRetrieveClipSuccess(result: ClipListResponse) {
-        errorMessage.value = null
-        clipListAdapter.setClipList(result)
-    }
-
-    private fun onRetrievePostMovieError() {
-        errorMessage.value = R.string.post_error
-    }
-
-    private fun onRetrievePostMovieSuccess(result: SingleMovieResponse?) {
-        singleMovie.value = result
-    }
-
-    private fun onRetrieveMovieFinish() {
-        loadingVisibility.value = View.GONE
-    }
-
-    private fun onRetrieveMovieStart() {
-        loadingVisibility.value = View.VISIBLE
-        errorMessage.value = null
-    }
-
 }
